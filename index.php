b57a02791a2f80a99ea803fb8aa8cbeeaeee2895
topiemie <topiemie@163.com>
2013-06-05 00:08:20 +0800

    update

--- index.php
+++ index.php
@@ -138,7 +138,33 @@
     </div> <!-- /container -->
     <script src="js/bootstrap-modalmanager.js"></script>
     <script src="js/bootstrap-modal.js"></script>
-    <script language="javascript">
+<script type="text/javascript">
+
+  $(function(){
+
+    $.fn.modalmanager.defaults.resize = true;
+
+    $('[data-source]').each(function(){
+      var $this = $(this),
+        $source = $($this.data('source'));
+
+      var text = [];
+      $source.each(function(){
+        var $s = $(this);
+        if ($s.attr('type') == 'text/javascript'){
+          text.push($s.html().replace(/(\n)*/, ''));
+        } else {
+          text.push($s.clone().wrap('<div>').parent().html());
+        }
+      });
+      
+      $this.text(text.join('\n\n').replace(/\t/g, '    '));
+    });
+
+//    prettyPrint();
+  });
+</script>
+<script language="javascript">
       function about(){
           var tmpl = [
     // tabindex is required for focus
